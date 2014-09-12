9a873ee894caa3051c79b5ed4b3f6b295da162ba
Sergey Fursov <GeyseR85@gmail.com>
2014-09-12 22:42:14 +0400

    make sorlery compatible with sorl.thumbnail==11.12.1b

--- sorlery/tasks.py
+++ sorlery/tasks.py
@@ -8,8 +8,13 @@ def create_thumbnail(file_, geometry_string, options, name):
     thumbnail = ImageFile(name, default.storage)
 
     if not thumbnail.exists():
-        source = ImageFile(file_)
+        if file_:
+            source = ImageFile(file_)
+        else:
+            return
         source_image = default.engine.get_image(source)
+        image_info = default.engine.get_image_info(source_image)
+        options['image_info'] = image_info
         size = default.engine.get_image_size(source_image)
         source.set_size(size)
         default.backend._create_thumbnail(source_image, geometry_string, options, thumbnail)
