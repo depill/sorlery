04ab06857f13d954cd7191aa4db8633e362569f7
Sergey Fursov <GeyseR85@gmail.com>
2014-09-16 15:43:33 +0400

    backward compatibility with sorl.thumbnail < 11.12.1b

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sorlery',
-    version='1.2',
+    version='1.3',
     description="",
     author="Aidan Lister",
     author_email='aidan@aidanlister.com',
--- sorlery/tasks.py
+++ sorlery/tasks.py
@@ -13,8 +13,11 @@ def create_thumbnail(file_, geometry_string, options, name):
         else:
             return
         source_image = default.engine.get_image(source)
-        image_info = default.engine.get_image_info(source_image)
-        options['image_info'] = image_info
+        try:
+            image_info = default.engine.get_image_info(source_image)
+            options['image_info'] = image_info
+        except AttributeError:
+            options['image_info'] = {}
         size = default.engine.get_image_size(source_image)
         source.set_size(size)
         default.backend._create_thumbnail(source_image, geometry_string, options, thumbnail)
