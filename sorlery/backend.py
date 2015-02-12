b5123923db9f4b22519865a840d842223ccc4e6f
Davíð Fannar Gunnarsson <david@skjarinn.is>
2015-02-12 15:54:39 +0000

    Change so thumbnails generate in the background while not disturbign the user.

--- sorlery/backend.py
+++ sorlery/backend.py
@@ -1,12 +1,14 @@
-from django.utils import six
 from sorl.thumbnail.conf import settings, defaults as default_settings
 from sorl.thumbnail.images import ImageFile
-from sorl.thumbnail.kvstores.base import add_prefix
-from sorl.thumbnail.helpers import serialize, deserialize
+from sorl.thumbnail.images import DummyImageFile, ImageFile
 from sorl.thumbnail import default
 from sorl.thumbnail.base import ThumbnailBackend
+from sorl.thumbnail.parsers import parse_geometry
 
 from sorlery.tasks import create_thumbnail
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class QueuedThumbnailBackend(ThumbnailBackend):
@@ -14,62 +16,43 @@ class QueuedThumbnailBackend(ThumbnailBackend):
     Queue thumbnail generation with django-celery.
     """
     def get_thumbnail(self, file_, geometry_string, **options):
-        async = options.pop('async', True)
+        """
+        Returns thumbnail as an ImageFile instance for file with geometry and
+        options given. First it will try to get it from the key value store,
+        secondly it will create it.
+        """
 
-        if not async:
-            return super(QueuedThumbnailBackend, self).get_thumbnail(file_, geometry_string, **options)
+        if file_:
+            source = ImageFile(file_)
+        elif settings.THUMBNAIL_DUMMY:
+            return DummyImageFile(geometry_string)
+        else:
+            return None
 
-        source = ImageFile(file_)
+        # preserve image filetype
+        if settings.THUMBNAIL_PRESERVE_FORMAT:
+            options.setdefault('format', self._get_format(source))
 
-        for key, value in self.default_options.iteritems():
+        for key, value in self.default_options.items():
             options.setdefault(key, value)
 
+        # For the future I think it is better to add options only if they
+        # differ from the default settings as below. This will ensure the same
+        # filenames being generated for new options at default.
         for key, attr in self.extra_options:
             value = getattr(settings, attr)
             if value != getattr(default_settings, attr):
                 options.setdefault(key, value)
 
-        # Generate a name for the thumbnail
         name = self._get_thumbnail_filename(source, geometry_string, options)
-
-        # See if we've got a hit in the cache
         thumbnail = ImageFile(name, default.storage)
         cached = default.kvstore.get(thumbnail)
+
         if cached:
             return cached
 
-        # We cannot check if the file exists, as remote storage is slow. If
-        # we have reached this point, the image does not exist in our kvstore
-        # so create the entry and queue the generation of the image.
-        #
-        # Note: If the thumbnail file has been deleted, you will need to manually
-        # clear the corresponding row from the kvstore to have thumbnail rebuilt.
-        job = create_thumbnail.delay(file_, geometry_string, options, name)
-        if isinstance(file_, six.string_types):
-            filename = file_.split('/')[-1]
-        else:
-            filename = file_.name
+        job = create_thumbnail.delay(file_, geometry_string, options)
+
         if job:
-            geometry = (0, 0)
-            # We can't add a source row to the kvstore without the size
-            # information being looked up, so add dummy information here
-            # We'll need to correct this information when we generate the thumbnail
-            source.set_size(geometry)
-            default.kvstore.get_or_set(source)
-
-            # We don't want to do any file access in this thread, so we tell sorlery
-            # to proceed as normal and cheekily update the name and storage after
-            # the hash has been calculated.
-            thumbnail.set_size(geometry)
-            default.kvstore.set(thumbnail, source)
-
-            # Now we go back and manually update the thumbnail to point at the source image
-            # Hopefully someone can suggest a better way to do this ... but the sorl internals
-            # don't make it easy to.
-            rawvalue = default.kvstore._get_raw(add_prefix(thumbnail.key))
-            rawvaluearr = deserialize(rawvalue)
-            rawvaluearr['name'] = filename
-            default.kvstore._set_raw(add_prefix(thumbnail.key), serialize(rawvaluearr))
-
-        thumbnail.name = filename
-        return thumbnail
+            source.set_size(parse_geometry(geometry_string))
+            return source
--- sorlery/tasks.py
+++ sorlery/tasks.py
@@ -4,28 +4,19 @@ from sorl.thumbnail.images import ImageFile
 
 
 @shared_task
-def create_thumbnail(file_, geometry_string, options, name, force=False):
+def create_thumbnail(file_, geometry_string, options):
+    source = ImageFile(file_)
+
+    name = default.backend._get_thumbnail_filename(source, geometry_string, options)
     thumbnail = ImageFile(name, default.storage)
 
-    if thumbnail.exists() and not force:
-        return
-
-    if file_:
-        source = ImageFile(file_)
-    else:
-        return
     source_image = default.engine.get_image(source)
-    try:
-        image_info = default.engine.get_image_info(source_image)
-        options['image_info'] = image_info
-    except AttributeError:
-        options['image_info'] = {}
+    image_info = default.engine.get_image_info(source_image)
+
+    options['image_info'] = image_info
     size = default.engine.get_image_size(source_image)
     source.set_size(size)
-
     default.backend._create_thumbnail(source_image, geometry_string, options, thumbnail)
-    if hasattr(default.backend, '_create_alternative_resolutions'):
-        default.backend._create_alternative_resolutions(source_image, geometry_string, options, thumbnail.name)
 
     # Need to update both the source and the thumbnail with correct sizing
     default.kvstore.set(source)
